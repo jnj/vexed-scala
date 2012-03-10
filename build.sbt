34c7c36fa8e1429ff6c59ddabedd89ac2e011859
Josh Joyce <jnjoyce@speakeasy.net>
2012-03-09 21:47:22 -0600

    upgrade sbt

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "vexed-scala"
+
+organization := "org.joshjoyce"
+
+scalaVersion := "2.9.1"
+
+version := "0.0.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.7.1"
--- project/build/VexedProject.scala
+++
@@ -1,5 +0,0 @@
-import sbt.{ProjectInfo, DefaultProject}
-
-class VexedProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalaTest = "org.scalatest" % "scalatest" % "1.2"    
-}
